3a7644226ec9611e7245a1c82641fd6189d685d6
qftWayfarer <ren.pankovich@gmail.com>
2025-01-02 20:54:21 -0500

    added function to generate coordinates for the check operators

--- rotated_surface_code_coordinates.py
+++ rotated_surface_code_coordinates.py
@@ -16,3 +16,44 @@ def rsurf_qubit_coords(L:int) -> Set[Tuple[int,int]]:
         
         return Lset
 
+def rsurf_check_coords(L:int) -> List[Set[Tuple[int,int]]]:
+
+    if L==3:
+        xcheck_coords = {(-1,1), (1,3), (3,1), (5,3)}
+
+        zcheck_coords = {(1,1),(1,5),(3,-1), (3,3)}
+
+        return [xcheck_coords,zcheck_coords]
+    
+    else:
+        xcheck_coords,zcheck_coords = rsurf_check_coords(L-2)
+
+        z1 = (1,2*L-1)
+        z2 = (2*L-3,-1)
+        dy = [-2,2]
+
+        zcheck_coords.add(z1)
+        zcheck_coords.add(z2)
+
+        for ii in range(L-2):
+            z1 = (z1[0]+2,z1[1]+dy[ii%2])
+            z2 = (z2[0]+dy[ii%2],z2[1]+2)
+
+            zcheck_coords.add(z1)
+            zcheck_coords.add(z2)            
+
+        x1 = (-1,2*L-5)
+        x2 = (2*L-3,1)
+        dx = [2,-2]
+
+        xcheck_coords.add(x1)
+        xcheck_coords.add(x2)
+
+        for ii in range(L-2):
+            x1 = (x1[0]+2, x1[1]+dx[ii%2])
+            x2 = (x2[0]+dx[ii%2],x2[1]+2)
+            xcheck_coords.add(x1)
+            xcheck_coords.add(x2)
+
+        return xcheck_coords,zcheck_coords
+
