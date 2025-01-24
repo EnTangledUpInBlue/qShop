f579c306979898903d5d5161c666278627e8fabb
qftWayfarer <ren.pankovich@gmail.com>
2025-01-24 17:22:16 -0500

    added dictionary building functions modified neighborhood builder to reflect standard surf code

--- standard_surface_code_coordinates.py
+++ standard_surface_code_coordinates.py
@@ -66,3 +66,75 @@ def surf_check_coords(L:int) -> List[Set[Tuple[int,int]]]:
             xcheck_coords.add(x2)
 
         return [xcheck_coords,zcheck_coords]
+
+def surf_q2i(L:int) -> Dict[Tuple[int,int],int]:
+    r"""
+    Creates a dictionary mapping qubit coordinates to integer labels. Note that this mapping is based on the lowest integer label and 
+    is different than might be expected based on geometry of the lattice.
+
+    :param L: The dimensions of the square lattice for the rotated surface code.
+    
+    """
+    qcoords = surf_qubit_coords(L)
+
+    q2i:Dict[Tuple[int,int],int] = {q: i for i,q in enumerate(sorted(qcoords, key=lambda v:(v[0],v[1])))}
+
+    return q2i
+
+def surf_c2i(L:int) -> Dict[bool,Dict[Tuple[int,int],int]]:
+    r"""
+    Creates a dictionary mapping check coordinates to integer labels.
+
+    :param L: The dimensions of the square lattice for the rotated surface code.
+    
+    """
+    xcoords,zcoords = surf_check_coords(L)
+
+    x2i:Dict[Tuple[int,int],int] = {x: i for i,x in enumerate(sorted(xcoords,key = lambda v:(v[0],v[1])))}
+
+    z2i:Dict[Tuple[int,int],int] = {z: i for i,z in enumerate(sorted(zcoords,key = lambda v:(v[0],v[1])))}
+
+    return {False:x2i,True:z2i}
+
+
+def surf_stabilizer_generators(L:int) -> List[List[Set[int]]]:
+    r"""
+    Function to use specify the set of generating operators for the rotated surface code.
+    Qubits are labeled according to the function q2i.
+
+    :param L: The dimensions of the square lattice for the rotated surface code.
+    
+    """
+    q2i = surf_q2i(L)
+    c2i = surf_c2i(L)
+
+    stabs = []
+
+    for sector in [False,True]:
+        sect_stabs = []
+        for check in c2i[sector]:
+            chk = set()
+            for pot_q in potential_nbrhd(check):
+                if pot_q in q2i:
+                    chk.add(q2i[pot_q])
+            sect_stabs.append(chk)
+        stabs.append(sect_stabs)
+
+    return stabs
+
+
+def potential_nbrhd(r:Tuple[int,int]) -> List[Tuple[int,int]]:
+    r"""
+    Function for producing a set of coordinates neighboring the input tuple r
+
+    """
+
+    nbrs = []
+
+    deltas = [1,-1]
+
+    for x in deltas:
+        nbrs.append((r[0]+x,r[1]))
+        nbrs.append((r[0],r[1]+x))
+
+    return nbrs
